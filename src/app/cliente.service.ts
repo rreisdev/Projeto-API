d4147f2c37b162a532687450e4929f01a14dbffa
rreisdev <rreis.dev@hotmail.com>
2023-05-30 20:26:46 -0300

    atualizacao

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -9,12 +9,32 @@ import { Cliente } from './models/Cliente.model';
 })
 export class ClienteService {
 
-  url: string = "http://localhost:3000/clientes";
+  url: string = "http://localhost:4200/clientes";
+
 constructor(private _httpClient:HttpClient) { }
 
 getClientes(): Observable<Cliente[]>{
 return this._httpClient.get<Cliente[]>(this.url);
 }
 
-  constructor() { }
+cadastrarCliente(cliente: Cliente):Observable<Cliente[]>{
+  return this._httpClient.post<Cliente[]>(this.url, cliente);
+  }
+
+getCliente(id:any):Observable<Cliente[]>{
+  const urlListarUm = `${this.url}?id=${id}`;
+  return this._httpClient.get<Cliente[]>(urlListarUm);
+  }
+  atualizarCliente(id: any, cliente: Cliente):Observable<Cliente[]>{
+  const urlAtualizar = `${this.url}/${id}`;
+  return this._httpClient.put<Cliente[]>(urlAtualizar, cliente);
+  }
+
+  excluirCliente(id:any):Observable<Cliente[]>{
+    const urlExcluir = `${this.url}/${id}`;
+    return this._httpClient.delete<Cliente[]>(urlExcluir);
+    }
+    
+    
+
 }
